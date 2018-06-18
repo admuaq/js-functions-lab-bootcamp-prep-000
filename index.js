72011ffbcdf3c986391d2306400ea3eb318f5057
Adham Muhammad <admuaq@gmail.com>
2018-06-18 11:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,3 @@
 function happyHolidays () {
   return "Happy holidays!"
 }
-
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
-}
-
-function holidayCountdown(days, holiday) {
-  return 'It\'s ${days} days until ${holiday}!';
-}
