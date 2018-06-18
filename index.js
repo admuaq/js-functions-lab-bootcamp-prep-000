b3b65feab9b56845049bde2cccb10e5a1e492a00
Adham Muhammad <admuaq@gmail.com>
2018-06-18 12:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidaysTo(name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return 'It\'s ${days} days until ${holiday}!';
+  return 'It\'s ' + days + ' until ' + holiday + '!';
 }
